fd55726a7adc27aafd6105ee5c57c91733f06daf
AndrewKushnir <cuckop30@gmail.com>
2023-03-25 20:09:24 +0300

    sdasd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-const randomCount = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
+const randomCount = (min, max) =>
+  Math.floor(Math.random() * (max + 1 - min) + min);
 const checkStrLength = (str, maxLenght) => str.length <= maxLenght;
 
+console.log("assda");
