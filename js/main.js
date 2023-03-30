3beb32c89ac6c4990966082cbb5923797bcb69b4
AndrewKushnir <cuckop30@gmail.com>
2023-03-30 16:27:09 +0300

    task5

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-const randomCount = (min, max) =>
-  Math.floor(Math.random() * (max + 1 - min) + min);
-const checkStrLength = (str, maxLenght) => str.length <= maxLenght;
-
-let massWithUsersPhoto = [];
-
-class UsersPhotos{
-  constructor(id, description, likes, comments){
-    this.id = id;
-    this.url = `photos/${id}.jpg`;
-    this.description = description;
-    this.likes = likes;
-    this.comments = comments;
-  }
-}
-
-function generateMassWithObjects(count) {
-  for (let i = 0; i < count; i++) {
-    let user = new UsersPhotos(
-      i + 1,
-      "some comment",
-      randomCount(15, 200),
-      randomCount(0, 200)
-    );
-    massWithUsersPhoto[i] = user;
-  }
-}
+import {generateMassWithObjects, massWithUsersPhoto} from './data.js';
 
 generateMassWithObjects(25);
-
