0d7513956283e4bae38d0d86b9c95f51e1202eed
AndrewRedFox <90340969+AndrewRedFox@users.noreply.github.com>
2023-05-10 15:20:09 +0300

    Правда или действие(нет)

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,17 @@
 const form = document.querySelector('#upload-select-image');
+const comment = document.querySelector('.text__description');
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__error-text'
 });
+
+function checkCommentLength(text){
+  return(text.length > 19 && text.length<141);
+}
+
+pristine.addValidator(comment, checkCommentLength(comment),'Comment length should not be more than 355 symbols and less than 10 symbols');
 
 form.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
